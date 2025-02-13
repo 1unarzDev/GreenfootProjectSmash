85b3a5294f9c588c9f6866cb4425afb55b15efc2
peekkoo <kriscallkepo@gmail.com>
2025-02-13 15:33:47 -0600

    Player Changes

--- Hitbox.java
+++ Hitbox.java
@@ -8,12 +8,13 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Hitbox extends Actor
 {
-    /**
-     * Act - do whatever the Hitbox wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    public Hitbox(Player player){
+        GreenfootImage Hitbox = getImage(   );
+        Hitbox.scale(player.getImage().getHeight(), player.getImage().getHeight());
+        setImage(Hitbox);
+    }
     public void act()
     {
-        // Add your action code here.
+        
     }
 }
