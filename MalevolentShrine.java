4b36c2c4586b752184f049639316987c4ac30cdd
lunarzDev <lunarzDev@outlook.com>
2025-02-18 10:12:37 -0600

    Hitbox

--- MalevolentShrine.java
+++ MalevolentShrine.java
@@ -4,6 +4,12 @@ public class MalevolentShrine extends Domain
 {
     public MalevolentShrine()
     {    
+        Paul paul_player = new Paul(100, 0);
+        Hitbox paul_hb = new Hitbox(paul_player);
         
+        GreenfootImage bg = new GreenfootImage("malevolentShrine.jpg");
+        
+        super.addObject(paul_player, 0, 0);
+        super.addObject(paul_hb, 0, 0);
     }
 }
--- Player.java
+++ Player.java
@@ -1,9 +1,7 @@
 import greenfoot.*;
 public class Player extends Actor
 {
-    private final int ULT_GAIN_SMALL = 5;
-    private final int ULT_GAIN_LARGE = 10;
-    private final int MOVE_SPEED = 5;
+    private final int ACCELERATION = 5;
     private final int PLAYER_HEIGHT = 300;
     private final int PLAYER_WIDTH = 100;
     private final double GRAVITY = 1;
@@ -15,7 +13,6 @@ public class Player extends Actor
     
     public Player(){
         Controller controller = new Controller(this);
-        this.getWorld().addObject(controller, this.getX(), this.getY());
     }
     
     public void act()
